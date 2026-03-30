0a25645272cff4f9591668699520749da6dd2556
unknown <highwayhellman@gmail.com>
2026-03-30 17:49:21 +0300

    thangs

--- task_manager/views.py
+++ task_manager/views.py
@@ -4,7 +4,19 @@ from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+from django.http import HttpResponse
+import rollbar
 
+def test_rollbar(request):
+    """Тест Rollbar на Render"""
+    rollbar.report_message('✅ Render + Rollbar работает!', 'info')
+    
+    try:
+        1 / 0
+    except Exception:
+        rollbar.report_exc_info()
+    
+    return HttpResponse('✅ Проверь Rollbar Dashboard через 30 сек!')
 
 class IndexView(TemplateView):
     template_name = 'task_manager/index.html'
